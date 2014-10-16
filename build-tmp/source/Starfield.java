34e082669d6e2da79765d2f8e82d360f157aeb7a
unknown <chchao1@697-334-Bywater.sf.k12.internal>
2014-10-16 10:17:24 -0700

    Paint.html

--- build-tmp/source/Starfield.java
+++ build-tmp/source/Starfield.java
@@ -1,6 +1,22 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Starfield extends PApplet {
+
 Particle [] particleList;
 
-void setup()
+public void setup()
 {
   background(255);
   size(1200, 800);
@@ -16,7 +32,7 @@ void setup()
   particleList[0] = new OddballParticle (x,y);
 }
 
-void draw()
+public void draw()
 {
   background(255);
   for (int i=1; i< particleList.length-1; i++) {
@@ -27,7 +43,7 @@ void draw()
   particleList[0].show();
 }
 
-void mousePressed() {
+public void mousePressed() {
   if (mouseButton == LEFT) {
     particleList = new Particle[500];
     colorMode(HSB);
@@ -89,9 +105,9 @@ class OddballParticle implements Particle
   }
 
   public void show() {
-  	fill(#9A56FF);
+  	fill(0xff9A56FF);
     float angle = TWO_PI / 5;
-    float halfAngle = angle/2.0;
+    float halfAngle = angle/2.0f;
     beginShape();
     for (float a = 0; a < TWO_PI; a += angle) {
       float sx = myX + cos(a) * 5;
@@ -102,5 +118,14 @@ class OddballParticle implements Particle
       vertex(sx, sy);
     }
     endShape(CLOSE);
+  }
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Starfield" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
   }
 }
